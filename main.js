86a20c9dc4cc7c63cae94b67f4ff57c8fb1661db
Prashanth-Reddy19 <kotha.prashanth02>
2022-11-25 14:21:39 +0530

    Dom ID

---
+++ main.js
@@ -0,0 +1,12 @@
+var headerTitle=document.getElementById("header-title")
+
+headerTitle.style.borderBottom='solid 5px #000'
+
+var things=document.getElementsByClassName("card card-body")
+console.log(things)
+
+main.style.color='green'
+main.style.fontWeight='bold'
+
+
+
