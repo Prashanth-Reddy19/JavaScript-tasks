3f823478028db90d58c2b750b69bcabbcf48830f
Prashanth-Reddy19 <kotha.prashanth02>
2022-11-25 16:00:09 +0530

    Dom Tagname

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 
 var items=document.getElementsByClassName('list-group-item')
 console.log(items)
-items[2].style.backgroundColor='green'
 for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
+    items[i].style.backgroundColor='pink'
 }
+
+
+var li=document.getElementsByTagName('li')
+console.log(li)
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='pink'
+}
+
+
 
 
