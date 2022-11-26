317996fc25b7e5d621993b8207b3e18827d230ed
Prashanth-Reddy19 <kotha.prashanth02>
2022-11-26 11:19:32 +0530

    Creating nodes and modifying Dom

--- main.js
+++ main.js
@@ -1,20 +1,70 @@
-// Query selector
+var itemList=document.querySelector('#items')
 
-var secondItem=document.querySelector('.list-group-item:nth-Child(2)')
-secondItem.style.backgroundColor='green'
+// //parentElement
+console.log(itemList.parentElement)
 
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)')
-thirdItem.style.display='none'
+// //lastelement child
+ console.log(itemList.lastElementChild)
+ itemList.lastElementChild.textContent='hello'
+
+// //last child
+console.log(itemList.lastChild)
+
+// // childNode
+
+console.log(itemList.childNodes)
+console.log(itemList.children)
+console.log(itemList.children[1])
+itemList.children[1].style.backgroundColor='blue'
+
+// // //first child
+
+ console.log(itemList.firstChild)
+
+//  //firstelement child
+ console.log(itemList.firstElementChild)
+ itemList.firstElementChild.textContent='hello'
+
+// //NextSibiling
+console.log(itemList.nextSibling)
+
+// // // NextSibing element
+console.log(itemList.nextElementSibling)
 
 
-//Query selectorAll
+// // previousSibiling
+console.log(itemList.previousSibling)
 
-var secondItem=document.querySelectorAll('li:nth-child(2)')
-console.log(secondItem[0])
-secondItem[0].style.color='green'
+// // //previous elementSibiling
+console.log(itemList.previousElementSibling)
+itemList.previousElementSibling.style.color='red'
  
-var odd=document.querySelectorAll('li:nth-child(odd)')
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green'
-}
+// //create element
+// // create div
 
+var newDiv=document.createElement('div')
+
+// //set attribute
+
+newDiv.setAttribute('title','hello Div')
+
+// // create textnode
+
+var newDivText=document.createTextNode('Hello World')
+
+// //Add text to div
+
+newDiv.appendChild(newDivText)
+var container=document.querySelector('header .container')
+var h1=document.querySelector('header h1')
+console.log(newDiv)
+container.insertBefore(newDiv,h1)
+//
+
+var newItems=document.createElement("items")
+var newItemsText=document.createTextNode('Hello World')
+newItems.appendChild(newItemsText)
+var listgroup=document.querySelector('items .list-group')
+var ul=document.querySelector('items ul')
+console.log(newItems)
+items.insertBefore(newItems,ul)
